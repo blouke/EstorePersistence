c9d9215b04d9557b5e2531fc98ea9b6b54c340f9
blouke <muhammad.atif.148@gmail.com>
2015-09-29 11:28:53 -0500

    Refactored

---
+++ src/main/java/com/estore/jpa/JpaTest.java
@@ -0,0 +1,35 @@
+package com.estore.jpa;
+import java.util.List;
+
+import javax.persistence.*;
+import com.estore.domain.product.Product;
+
+public class JpaTest {
+	private EntityManager manager;
+	 
+    public JpaTest(EntityManager manager) {
+        this.manager = manager;
+    }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("Estore");
+        EntityManager manager = factory.createEntityManager();
+        JpaTest test = new JpaTest(manager);
+ 
+        test.listProducts();
+ 
+        System.out.println(".. done");
+    }
+    
+    private void listProducts() {	
+		List<Product> resultList = manager.createQuery("Select a From Product a", Product.class).getResultList();
+		System.out.println("num of products:" + resultList.size());
+		for (Product next : resultList) {
+		    System.out.println("next product: " + next.getName());
+		}
+        
+    }
+ 
+}
