9a64d250a3b132dec38fb339c4e4ae5fac959c43
blouke <muhammad.atif.148@gmail.com>
2015-10-21 08:37:17 -0500

    Added data transfer object for Order entity

---
+++ src/main/java/com/estore/domain/order/OrderTO.java
@@ -0,0 +1,41 @@
+package com.estore.domain.order;
+
+import java.io.Serializable;
+
+public class OrderTO implements Serializable {
+	public int orderId;
+	public String orderDate;
+	public double orderTotal;
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public String getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+	public double getOrderTotal() {
+		return orderTotal;
+	}
+	public void setOrderTotal(double orderTotal) {
+		this.orderTotal = orderTotal;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public int getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+	public String orderStatus;
+	public int paymentId;
+}
