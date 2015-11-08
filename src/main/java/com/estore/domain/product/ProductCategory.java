d48810248caf24a6769a385ad048e1fdd6496755
Blair Douglass <blair.douglass@gmail.com>
2015-11-07 19:54:04 -0600

    Add attributes to stop data cacheing of products and categories

--- src/main/java/com/estore/domain/product/ProductCategory.java
+++ src/main/java/com/estore/domain/product/ProductCategory.java
@@ -5,12 +5,12 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table (name = "CATEGORY")
+@Cacheable(false)
 public class ProductCategory implements Serializable, IProductCategory {
 	private static final long serialVersionUID = 1L;
 	
